a62dcc27c8747f1ccd2375b0e28a9bcf7e2bad78
dhain <d.hain@gmx.at>
2023-04-19 23:49:17 +0200

    feat: implemented simple rust version

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    loop {
+        println!("Which version do you want to run?");
+        println!("\t(1) simple");
+        println!("\t(2) ");
+        
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).expect("Failed to read input");
+        
+        match input.trim() {
+            "1" => {
+                simple();
+                return Ok(());
+            },
+            "2" => {
+                println!("Not implemented yet!");
+                return Ok(());
+            },
+            _ => println!("Invalid input!\n")
+        }
+    }
+}
+
+fn simple() {
+    for number in 1..=100 {
+        let mut output = String::new();
+        if number % 3 == 0 {
+            output.push_str("Fizz");
+        }
+        if number % 5 == 0 {
+            output.push_str("Buzz");
+        }
+        if output.is_empty() {
+            output.push_str(&number.to_string());
+        }
+        
+        println!("{}", output);
+    }
 }
