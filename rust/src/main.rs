1d3792c815c6bf3eb64dd48b7cc13d92b2980a68
David Hain <57726396+d-hain@users.noreply.github.com>
2023-05-07 22:40:27 +0200

    Removed unnecessary code

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,31 +1,4 @@
-use anyhow::Result;
-
-fn main() -> Result<()> {
-    println!("FizzBuzz written in Rust.\n");
-
-    loop {
-        println!("Which version do you want to run?");
-        println!("\t(1) simple");
-        println!("\t(2) ");
-        
-        let mut input = String::new();
-        std::io::stdin().read_line(&mut input).expect("Failed to read input");
-        
-        match input.trim() {
-            "1" => {
-                simple();
-                return Ok(());
-            },
-            "2" => {
-                println!("Not implemented yet!");
-                return Ok(());
-            },
-            _ => println!("Invalid input!\n")
-        }
-    }
-}
-
-fn simple() {
+fn main() {
     for number in 1..=100 {
         let mut output = String::new();
         
